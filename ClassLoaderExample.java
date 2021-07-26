eb2183515bd2bc2349bae7dca8edfd1b6548abff
unknown <ustjavab217@iiht.tech>
2021-07-26 15:17:18 +0530

    commit

---
+++ ClassLoaderExample.java
@@ -0,0 +1,9 @@
+public class ClassLoaderExample
+{
+public static void main(String args[])
+{
+Class c=ClassLoaderExample.class;
+System.out.println(c.getClassLoader());
+System.out.println(String.class.getClassLoader());
+}
+}
